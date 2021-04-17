eff75de75b587c615cc56b289ccadd26e1ad2667
Malcolm Rebughini <malcolm.rebughini@gmail.com>
2021-04-17 12:17:24 -0700

    docs(*): initial outline for algo

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+// select random points form data to use as centers
+// group by proximity to centers
+// calculate new centers based on group
+// calculate distances again and reassign
+// continue until the grouping doesn't change anymore
+
+// Config struct
+// KMeans struct
+
 #[cfg(test)]
 mod tests {
     #[test]
