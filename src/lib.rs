bd7ad2d1efce1009d1bf4f9b495f54dfa4114638
Malcolm Rebughini <malcolm.rebughini@gmail.com>
2021-04-17 12:24:53 -0700

    feat(*): initial config and kmeans outline

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,47 @@
 // continue until the grouping doesn't change anymore
 
 // Config struct
+
+// Config keys based on sklearn interface
+struct Config {
+    n_clusters: u32,
+}
+
+impl Config {
+    pub fn default() -> Config {
+        let config = Config { n_clusters: 8 };
+
+        return config;
+    }
+
+    pub fn n_clusters(mut self, n_clusters: u32) -> Self {
+        self.n_clusters = n_clusters;
+        return self;
+    }
+}
+
+struct KMeans {
+    config: Config,
+}
+
+impl KMeans {
+    pub fn new(config: Config) -> KMeans {
+        return KMeans { config: config };
+    }
+
+    pub fn fit(self, data: Vec<(f64, f64)>) {}
+}
+
 // KMeans struct
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn fit() {
+        let config = Config::default();
+        let kmeans = KMeans::new(config);
+        kmeans.fit(vec![]);
     }
 }
